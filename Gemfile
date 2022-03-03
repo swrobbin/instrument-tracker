1eadcc4e8a2c06474f291741f75829f93286eddf
Stephen Robbins <swrobbin@gmail.com>
2022-03-03 15:26:49 -0500

    troubleshooting deployment issues

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 5.0.0'
 end
 
+
 group :test do
   gem 'rspec-json_expectations'
   gem 'shoulda-matchers', '~> 4.0'
--- db/migrate/20220117200508_change_column_names_in_instruments.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNamesInInstruments < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :instruments, :model_name, :name
-    rename_column :instruments, :brand_name, :brand
-  end
-end
